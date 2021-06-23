2aa6e8fecb0005c1af84b9feceb910580b2796a7
sun-hang <1844528595@qq.com>
2021-06-23 09:20:37 +0800

    test3.6

--- miniprogram/pages/addProductOption/addProductOption.js
+++ miniprogram/pages/addProductOption/addProductOption.js
@@ -5,9 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    optionPropLength: 0
+    optionPropLength: 0,
+    propList: [] //规格对象数组
   },
-  
+
   click() {
     const event = this.getOpenerEventChannel();
     event.emit('data', {
@@ -16,7 +17,7 @@ Page({
   },
 
   /**
-   * 
+   * 添加一条规格属性
    */
   addProp() {
     if (this.data.optionPropLength >= 2) {
@@ -27,6 +28,18 @@ Page({
     })
   },
 
+  /**
+   * 
+   * @param {*} e 
+   */
+  propDelClick(e) {
+    let index = e.currentTarget.dataset.item;
+    let propList = this.data.propList.splice(index, 1);
+    this.setData({
+      optionPropLength: this.data.optionPropLength - 1,
+      propList
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
