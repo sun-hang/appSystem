7eb4334a9670744f240545f8749ca4b28489d692
sun-hang <1844528595@qq.com>
2021-06-30 16:41:12 +0800

    test4.5

--- miniprogram/components/product/productList/productList.js
+++ miniprogram/components/product/productList/productList.js
@@ -4,16 +4,24 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    productList: {
+      type: Array
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    list: []
   },
-
+  observers: {
+    productList(list) {
+      this.setData({
+        list
+      })
+    }
+  },
   /**
    * 组件的方法列表
    */
